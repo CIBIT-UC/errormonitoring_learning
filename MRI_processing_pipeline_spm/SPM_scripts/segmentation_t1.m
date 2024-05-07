3095f3e25930363b6581ddacfff314a63c848efc
camilagdias <37186582+camilagdias@users.noreply.github.com>
2024-05-07 15:09:28 +0100

    Update segmentation_t1.m

--- MRI_processing_pipeline_spm/SPM_scripts/segmentation_t1.m
+++ MRI_processing_pipeline_spm/SPM_scripts/segmentation_t1.m
@@ -1,7 +1,7 @@
 % List of open inputs
 nrun = 1; % enter the number of runs here
 jobfile = ...
-    {'/DATAPOOL/home/cdias/errorMonitoring_fMRI/processing_pipeline_spm/SPM_scripts/segmentation_t1_job.m'};
+    {'/DATAPOOL/home/cdias/errorMonitoring_fMRI/MRI_processing_pipeline_spm/SPM_scripts/segmentation_t1_job.m'};
 jobs = repmat(jobfile, 1, nrun);
 inputs = cell(0, nrun);
 for crun = 1:nrun
