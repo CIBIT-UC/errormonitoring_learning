89ba82589c5e6c964aff55e7967b9339d06b217d
camilagdias <37186582+camilagdias@users.noreply.github.com>
2024-05-07 14:43:36 +0100

    Update motion_correction_unwarp.m

--- MRI_processing_pipeline_spm/SPM_scripts/motion_correction_unwarp.m
+++ MRI_processing_pipeline_spm/SPM_scripts/motion_correction_unwarp.m
@@ -1,7 +1,7 @@
 % List of open inputs
 nrun = 1; % enter the number of runs here
 jobfile = ...
-    {'/DATAPOOL/home/cdias/errorMonitoring_fMRI/processing_pipeline_spm/SPM_scripts/motion_correction_unwarp_job.m'};
+    {'/DATAPOOL/home/cdias/errorMonitoring_fMRI/MRI_processing_pipeline_spm/SPM_scripts/motion_correction_unwarp_job.m'};
 jobs = repmat(jobfile, 1, nrun);
 inputs = cell(0, nrun);
 for crun = 1:nrun
